e1c619b9db4cfabae19d33be0d9161c93394b874
Bob Jarvis <bob@zeal.co>
2023-09-22 11:58:20 +1000

    file watching python script for shutdown

---
+++ config/plants.watcher.py
@@ -0,0 +1,17 @@
+import os
+import time
+
+def watch_for_shutdown_file():
+    directory = os.path.dirname(os.path.realpath(__file__))
+    shutdown_file = os.path.join(directory, "shutdown.tmp")
+
+    while True:
+        if os.path.exists(shutdown_file):
+            os.remove(shutdown_file)
+            print("SHUTDOWN!")
+            os.system("shutdown /s /t 0")
+            break
+        time.sleep(5)
+
+if __name__ == "__main__":
+    watch_for_shutdown_file()
