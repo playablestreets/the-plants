5b6aadcd7a5f8c8c18e625a422f1ebbd89550ffe
Bob Jarvis <bob@zeal.co>
2023-09-22 14:36:16 +1000

    update updates

--- config/update.sh
+++ config/update.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 cd /home/pi/Desktop/the-plants
+echo "pulling from git"
 git pull
 cd config
+echo "copying scripts"
 ./copyscripts.sh
+sleep 5
+echo "rebooting in 5 seconds..."
 systemctl reboot
